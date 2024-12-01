f2ea76fc8ec30d33d11bf464ce9afff30e77c73f
codingninja2 <edwinafatsawo@gmail.com>
2024-12-01 18:16:16 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#this is my name
+print("Edwin Afatsawo")
+#this my age
+print("I am 13 years old")
+#my class
+print("im in the 8th grade")
+#Adding two numbers
+print("the result of 10+20 is",10+20)
+#multiplying two numbers
+print("the result of 10*20 is",10*20)
+#dividing numbers
+print("the result of 10/20 is",10/20)
+#subtracting two numbers
+print("the result of 10-20 is",10-20)
